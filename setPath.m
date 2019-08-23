c7e071a90d22c221da4e719f2ac6067a0b372f4d
Louis Everett <leverett@utep.edu>
2019-08-22 21:03:53 -0600

    Add files via upload

---
+++ setPath.m
@@ -0,0 +1,7 @@
+%path(genpath('C:\Users\leverett\Dropbox\Classes\MachineLearning\MatlabRecipes'),path)
+%path('C:\Users\Louis Everett\Dropbox\Classes\MachineLearning\MatlabRecipes\General',path)
+base = 'C:\Users\leverett\Dropbox\Classes\MatLabBasedClasses\MachineLearning\MatlabRecipes';
+path(strcat(base,'\General'),path)
+path(strcat(base,'\Chapter8'),path)
+path(strcat(base,'\Chapter10'),path)
+path(genpath(strcat(base,'\Chapter9')),path)
